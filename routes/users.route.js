670ffdbc3c01624cd8bceecf09d71c14fefea078
unknown <you@example.com>
2021-11-20 21:45:42 +0200

    some msg

--- routes/users.route.js
+++ routes/users.route.js
@@ -2,17 +2,35 @@ const express = require("express");
 const userController = require("../controllers/bank.controller");
 const router = express.Router();
 
-router
-  .get("/", (req, res) => {
-    userController.getAllUsers(req, res);
-  })
+
+router.get("/", (req, res) => {
+  userController.getAllUsers(req, res);
+})
   .post("/", (req, res) => {
     userController.addNewUser(req, res);
   })
-  .put("/:id", (req, res) => {
+  .put("/updateUser/:id", (req, res) => {
     userController.updateUser(req, res);
+  })
+  .put("/updateCredit", (req, res) => {
+    userController.updateCredit(req, res);
+  })
+  .put("/deposit", (req, res) => {
+    userController.updateMoney(req, res);
+  })
+  .put("/withDrawMoney", (req, res) => {
+    userController.withDrawMoney(req, res);
+  })
+  .put("/Transferring", (req, res) => {
+    userController.Transferring(req, res);
   })
   .delete("/:id", (req, res) => {
     userController.deleteUser(req, res);
   });
+
+
+
 module.exports = router;
+
+
+
